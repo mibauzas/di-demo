e1770a134a0809db49e5038604f570df9a535416
Miquel Bauza Soriano <mibauzas@gmail.com>
2020-01-24 11:56:28 +0100

    Use of default profile

--- src/main/java/com/mibauzas/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/mibauzas/didemo/services/PrimaryGermanGreetingService.java
@@ -4,16 +4,16 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 /**
- * Created by mbs on 01/23/20
+ * Created by mbs on 01/24/20
  */
 @Service
+@Profile("de")
 @Primary
-@Profile({"en","default"})
-public class PrimaryGreetingService implements GreetingService{
+public class PrimaryGermanGreetingService implements GreetingService{
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return "Primärer Grussdienst";
     }
 
 }
